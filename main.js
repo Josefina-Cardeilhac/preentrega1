5f5b4d11b5b378be6fafa8c0569d1f1e86ccd56b
Josefina Cardeilhac <cardeilhac.josefina@gmail.com>
2024-06-10 23:25:15 -0300

    entrega1

---
+++ main.js
@@ -0,0 +1,9 @@
+class Product {
+    constructor(nombre,precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+}
+ let shampoo =new Product("shampoo de avena", 6000);
+ console.log(shampoo)
