6217c7c8453b24e27fc153c3d60a0f75b733e243
Josefina Cardeilhac <cardeilhac.josefina@gmail.com>
2024-07-09 21:28:05 -0300

    entrega3

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 class ProductManager {
-
+    static ultId = 0;
+    
     constructor() {
         this.products =[]
     }
 
     addProduct(title,description,price,img,code,stock){
-        if (!title || !description || !price || !img || !code || !stock )
-            {
-                console.log("todos los campos son obligatorios")
-                
+        if (!title || !description || !price || !img || !code || !stock ){
+                console.log("todos los campos son obligatorios");
+                return;
+            }
+            if (this.products.some(item=> item.code === code)) {
+                console.log("El codigo debe ser unico");
+                return;
+            }
+            const nuevoProducto = {
+                id: ProductManager.ultId,
+                title, 
+                description,
+                price,
+                img,
+                code,
+                stock
+            }
+
+            this.products.push(nuevoProducto)
+            getProducts(){
+                return this.products
             }
     }
 
